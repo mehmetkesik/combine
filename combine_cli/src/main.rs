8d717b110bfe834a15f57c334546d13f5f2fbcd5
mehmetkesik <livered31@gmail.com>
2021-09-06 22:53:42 +0300

    first commit

--- combine_cli/src/main.rs
+++ combine_cli/src/main.rs
@@ -5,27 +5,26 @@
 use combine_attributes::*;
 
 #[combine_fn]
-fn index(){
+fn index() {
+    println!("index çalıştı.");
 }
 
 fn main() {
-  let a = A{body:"xyz".to_string()};
-  println!("Hello, world! {}",a.add_br("heyyyy".to_string(),5,7.5));
+    let a = A { body: "xyz".to_string() };
+    println!("Hello, world! {}", a.add_br("heyyyy".to_string(), 5, 7.5));
 }
 
 #[combine_type]
 struct A {
-  body:String,
+    body: String,
 }
 
 impl A {
-  #[combine_fn]
-  fn add_br(&self,render_body:String,i:i32,f:f64) -> String {
-      render_body
-  }
+    #[combine_fn]
+    fn add_br(&self, render_body: String, i: i32, f: f64) -> String {
+        render_body
+    }
 
-  #[combine_fn]
-  fn send_http_get_request(&self,path:String) {
-    
-  }
+    #[combine_fn]
+    fn send_http_get_request(&self, path: String) {}
 }
